078111f59193c3360dc7e9d4891410e9847801ec
kyungje <cnyna@naver.com>
2020-08-12 14:12:15 +0900

    first commit

---
+++ src/main/java/local/PolicyHandler.java
@@ -0,0 +1,38 @@
+package local;
+
+import local.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+
+    @Autowired
+    DeliveryRepository deliveryRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onStringEventListener(@Payload String eventString){
+
+    }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverOrdered_Ship(@Payload Ordered ordered){
+
+        if(ordered.isMe()){
+            if(ordered.isMe()){
+                //물류사와의 전문교환
+                //고객센터에 SMS발송
+                Delivery delivery = new Delivery();
+                delivery.setOrderId(ordered.getId());
+                delivery.setStatus("SHIPPED");
+
+                deliveryRepository.save(delivery);
+            }
+        }
+    }
+
+}
